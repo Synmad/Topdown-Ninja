aa35403fc2300299cc8d9776c4972df589ece87b
Synmad <lucasalumlucasalumlucasalum@gmail.com>
2023-06-05 12:47:27 -0300

    v0.0.3
    Animación de movimiento del jugador

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,17 +3,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
-    Rigidbody2D rb;
-    Vector2 movement;
 
+    Rigidbody2D rb;
+    Animator animator;
+    Vector2 movement;
+    
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        movement.x = Input.GetAxisRaw("Horizontal"); movement.y = Input.GetAxisRaw("Vertical");
+        movement.Normalize();
+
+        animator.SetFloat("Horizontal", movement.x); animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.magnitude);
     }
     private void FixedUpdate()
     {
