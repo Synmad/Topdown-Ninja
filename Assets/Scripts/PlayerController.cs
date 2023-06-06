29b6a9cbc1b1ef1fe517e68cbff848186758f284
Synmad <lucasalumlucasalumlucasalum@gmail.com>
2023-06-06 19:33:11 -0300

    v0.0.4
    Script de lanzamiento de shuriken

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float moveSpeed;
+    [SerializeField] float attackForce;
 
     Rigidbody2D rb;
     Animator animator;
     Vector2 movement;
+    [SerializeField] GameObject shurikenprefab;
+    [SerializeField] Transform attackpoint;
     
-    private void Awake()
+    void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
@@ -20,9 +23,20 @@ public class PlayerController : MonoBehaviour
 
         animator.SetFloat("Horizontal", movement.x); animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.magnitude);
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            ThrowShuriken();
+        }
     }
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+    void ThrowShuriken()
+    {
+        GameObject shuriken = Instantiate(shurikenprefab, attackpoint.position, Quaternion.identity);
+        shuriken.GetComponent<Rigidbody2D>().AddForce(attackpoint.right * attackForce);
+
     }
 }
