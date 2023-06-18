78de25a793dd126dd6125512731e8eff2b687bcc
Synmad <lucasalumlucasalumlucasalum@gmail.com>
2023-06-18 12:21:34 -0300

    v0.0.8

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -14,19 +14,19 @@ public class PlayerAttack : MonoBehaviour
         attackpoint = GameObject.Find("AttackPoint").transform;
         cam = Camera.main;
     }
-    public void Shuriken()
-    {
-        GameObject shuriken = Instantiate(shurikenprefab, attackpoint.position, Quaternion.identity);
-        shuriken.GetComponent<Rigidbody2D>().AddForce(attackpoint.up * attackForce, ForceMode2D.Impulse);
-    }
     public void Aim()
     {
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 
         Vector2 aimPos = mousePos - attackpoint.position;
-        float angle = Mathf.Atan2(aimPos.y, aimPos.x) * Mathf.Rad2Deg -90;
-        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        attackpoint.rotation = Quaternion.Slerp(attackpoint.rotation, rotation, 1);
-    }
+        float angle = Mathf.Atan2(aimPos.y, aimPos.x) * Mathf.Rad2Deg - 90;
 
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        attackpoint.rotation = rotation;
+    }
+    public void Shuriken()
+    {
+        GameObject shuriken = Instantiate(shurikenprefab, attackpoint.position, Quaternion.identity);
+        shuriken.GetComponent<Rigidbody2D>().AddForce(attackpoint.up * attackForce, ForceMode2D.Impulse);
+    }
 }
